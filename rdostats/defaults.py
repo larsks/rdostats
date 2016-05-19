811702470923ec9b5b5c2d70bd1d15a3c24b58f9
Lars Kellogg-Stedman <lars@redhat.com>
2016-05-19 11:39:20 -0400

    relocate repository

--- rdostats/defaults.py
+++
@@ -1,8 +0,0 @@
-product = 'RDO'
-url = 'https://bugzilla.redhat.com'
-
-status_open = ['NEW', 'ASSIGNED', 'ON_DEV']
-status_fixed = ['MODIFIED', 'POST', 'ON_QA']
-status_all = status_open + status_fixed
-template_dir = './templates'
-output_dir = './reports'
